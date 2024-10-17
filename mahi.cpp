4334c840563b558b938dbbfa640bde0801c93b7f
Dynamicmahesh7890 <p.pattanaik.mahesh7890@gmail.com>
2024-10-17 12:16:57 +0530

    Add files via upload

---
+++ mahi.cpp
@@ -0,0 +1,9 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+    char var1='l',var2='lit';
+    cout<<"the value of var1="<<var1<<endl;
+    cout<<"the value of var2="<<var2<<endl;
+    cout<<"the size of char"<<sizeof(var1)<<endl;
+}
