5876006413b71a7db351abee2f21088b48ad1428
Dynamicmahesh7890 <p.pattanaik.mahesh7890@gmail.com>
2024-10-31 00:43:10 +0530

    Add files via upload

---
+++ marko.cpp
@@ -0,0 +1,9 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+    int no1 ,no2;
+    cout<<"Enter the number"<<endl;
+    cin>> no1 >>no2;
+    no2 > no1 ? cout<<"no2 is max"<<endl: cout<<"no1 is max"<< endl;
+}
