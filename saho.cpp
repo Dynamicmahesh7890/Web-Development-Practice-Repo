1d0e3594c556d8f55f9d47c20801691ebae28e77
Dynamicmahesh7890 <p.pattanaik.mahesh7890@gmail.com>
2024-11-01 02:04:07 +0530

    Add files via upload

---
+++ saho.cpp
@@ -0,0 +1,24 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+    int ch;
+    cout<<"select any number from 1 to 3"<<;
+    cin>>ch;
+    switch(ch)
+    {
+        case1 :
+            cout<<"hello good morning all"<<endl;
+            break;
+        case2:
+            cout<<"Heloo good after noon"<<endl;
+            break;
+            
+        case3:
+            cout<<"Hello good evening"<<endl;
+            break;
+            defult:
+            cout<<"choice is so bad"<<endl;
+    }
+
+}
