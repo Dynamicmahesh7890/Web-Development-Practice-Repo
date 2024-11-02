262f1ba1c707d2abade4a24f66fdc90a3dcb4987
Dynamicmahesh7890 <p.pattanaik.mahesh7890@gmail.com>
2024-11-03 00:10:56 +0530

    Add files via upload

---
+++ reymond.cpp
@@ -0,0 +1,15 @@
+#include "iostrem"
+using namespace std;
+int main()
+{
+                bool no=71;//no is the var of bool;
+        cout<<"value of no"<< no <<endl;
+                bool no1=56;
+        cout<<"value of no1"<<no1<<endl;
+                bool no2=-78;
+        cout<<"value of no2"<<no2<<endl;
+                bool no3=89;
+        cout<<"value of no3"<<no3<<endl;
+                bool no4=false;
+        cout<<"value of no4"<<no4<<endl;
+}
