f7ad9daf56966b32aa26aea7630b487906dc893b
Dynamicmahesh7890 <p.pattanaik.mahesh7890@gmail.com>
2024-11-14 00:05:37 +0530

    Add files via upload

---
+++ master.cpp
@@ -0,0 +1,10 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+        cout<<" i am bbsr from print local var"<<x<<endl;
+}
+int main()
+{
+        cout<<" i have never with you"<<endl;
+}
